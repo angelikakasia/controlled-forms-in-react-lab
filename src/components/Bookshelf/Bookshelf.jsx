644e65d3d4853a1f295995a3acd9b1e3c258d094
suzanaai <angelska327@gmail.com>
2026-01-08 18:15:23 -0600

    finito

---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const Bookshelf = () => {
+  const [books, setBooks] = useState([]); //array
+  const [newBook, setNewBook] = useState({
+    //object
+    title: "",
+    author: "",
+  });
+  console.log(books);
+  const handleInputChange = (event) => {
+    // const name =event.target.name;
+    // const value =event.target.value;
+    const { name, value } = event.target;
+
+    setNewBook({
+      ...newBook,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault(); // the page doesnt do the full refresh just the component to refresh
+    const copyBooks = [...books, newBook]; // copy of the books and it is adding the new book
+
+    setBooks(copyBooks); //update the set of books
+  };
+  return (
+    <div className="bookshelfDiv">
+      <p>hello</p>
+      <h3>Add a Book</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          value={newBook.title}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="author"
+          value={newBook.author}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Add Book</button> 
+        {/* map =it takes from an array and you can change it to whatever you want */}
+      </form>
+
+      {
+        books.map((book, idx) => (   
+        
+            <div key= {idx}> 
+                <p> {book.title} </p>
+                <p> {book.author} </p>
+            </div>
+        ))
+      } 
+    </div>
+  );
+};
+export default Bookshelf;
